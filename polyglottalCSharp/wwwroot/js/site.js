9edf965ba68e3395bdf2e027bbd19b82c765c6b1
Code Chrysalis <codechrysalis@Codes-Mac-mini.local>
2018-08-30 17:52:06 +0900

    added moving clouds

--- polyglottalCSharp/wwwroot/js/site.js
+++ polyglottalCSharp/wwwroot/js/site.js
@@ -4,4 +4,18 @@
 // Write your JavaScript code.
 $(function() {
     console.log( "ready!" );
+    zoomin(50, 3, 1100);
 });
+
+function zoomin(interval, sizeChangeEveryInterval, stopResizeWhenSizeReached){
+    var timeout = setInterval(function(){
+        var target = $(".zoom-in")
+        var bgposx = target.css('background-position-x') ? parseInt(target.css('background-position-x')) : 0;
+        target.css('background-position-x',(bgposx - sizeChangeEveryInterval) + "px");
+        var bgposy = target.css('background-position-y') ? parseInt(target.css('background-position-y')) : 0;
+        target.css('background-position-y',(bgposy - sizeChangeEveryInterval) + "px");
+        var bgsize = !isNaN(parseInt(target.css('background-size'))) ? parseInt(target.css('background-size')) : 100;
+        target.css('background-size',(bgsize + sizeChangeEveryInterval) + "px");
+        if(stopResizeWhenSizeReached < bgsize) clearInterval(timeout)
+    }, interval)
+}
